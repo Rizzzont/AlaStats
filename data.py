35c57578ed2d2d19379add73f3ea5ab67fd50b71
Rizzzont <rizzzont@gmail.com>
2025-03-03 01:18:50 +0300

    Hi

--- data.py
+++ data.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 df = pd.read_excel("данные.xlsx", sheet_name="Товары")
 
-print("Hello")
+print("Hi")
 df.drop(columns=["Артикул WB", "Удаленный товар", "Доля карточки в выручке",
                  "Доля карточки в выручке (предыдущий период)", "Переходы в карточку"])
 
