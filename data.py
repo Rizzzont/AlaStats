fca308ea00fa8f828008d1825604d89458ea27e9
Rizzzont <rizzzont@gmail.com>
2025-03-03 01:27:19 +0300

    Alastats top

--- data.py
+++ data.py
@@ -2,7 +2,8 @@ import pandas as pd
 
 df = pd.read_excel("данные.xlsx", sheet_name="Товары")
 
-print("Hello")
+print("Hi")
+print("Alastats top")
 df.drop(columns=["Артикул WB", "Удаленный товар", "Доля карточки в выручке",
                  "Доля карточки в выручке (предыдущий период)", "Переходы в карточку"])
 
