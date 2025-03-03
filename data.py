6e3545977864ee000491ac0e5654e336d487650f
Rizzzont <rizzzont@gmail.com>
2025-03-03 23:03:00 +0300

    Добавил инфу о дизайнерах

--- data.py
+++ data.py
@@ -2,16 +2,25 @@ import pandas as pd
 
 df = pd.read_excel("данные.xlsx", sheet_name="Товары")
 
-print("Hi")
-print("Alastats top")
-df.drop(columns=["Артикул WB", "Удаленный товар", "Доля карточки в выручке",
-                 "Доля карточки в выручке (предыдущий период)", "Переходы в карточку"])
+data_about_designers = {"www": 0, "van": 0, "uuu": 0, "mmm": 0, "ccc": 0, "mvm": 0, "ddd": 0, "bas": 0, "aga": 0,
+                        "apa": 0, "deb": 0, "lll": 0, "eee": 0, "kkk": 0, "ooo": 0, "ppp": 0, "m": 0, "pv": 0, "rrr": 0,
+                        "rpr": 0, "vaa": 0, "e": 0, "yyy": 0}
 
-if 1 == 1:
-    print("Hello!")
+choose = ["Заказали, шт", "Доля карточки в выручке"]
 
-for i in range(5):
-    print("Alastats Top")
+choose_from_site = int(input("Представим что это кнопки на сайте ( 1 - сколько заказали от дизайнера,"
+                             "2 - его доля в выручке): ")) - 1
 
-print("Hello, World!")
-print(df)
+for index, row in df.iterrows():
+    article = row["Артикул продавца"]
+    quantity = row[choose[choose_from_site]]
+
+    temp = article.split(sep="-")
+    for part in temp:
+        key = part.lower()
+        if key in data_about_designers:
+            data_about_designers[key] += quantity
+
+    for part in data_about_designers:
+        data_about_designers[part] = round(data_about_designers[part], 2)
+print(data_about_designers)
---
+++ test.py
@@ -0,0 +1,8 @@
+data = {"AAA": 0,
+        "DDD": 0}
+
+
+temp = "AKR-3MM-BRL-6982-MMM-4cm-X2".split(sep="-")
+
+if "AAA" in data:
+    print(temp)
